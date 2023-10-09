40729da92f980f7371a5a9061842299afb358e28
kushmathapa <wingskushma@gmail.com>
2023-10-09 19:32:14 +0545

    chore: using router name

--- src/main.js
+++ src/main.js
@@ -16,14 +16,22 @@ const router = createRouter({
       redirect: '/teams',
     },
     {
+      name: 'teams',
       path: '/teams',
       component: TeamsList,
-      children: [{ path: ':teamId', component: TeamMembers, props: true }],
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
     },
     {
       path: '/users',
       component: UsersList,
-    }
+    },
     {
       path: '/:404NotFound(.*)',
       component: NotFound,
