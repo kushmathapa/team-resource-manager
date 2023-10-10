edad464ff1b71a7e207a9f8b80a77d7e965c6117
kushmathapa <wingskushma@gmail.com>
2023-10-10 18:07:00 +0545

    chore: add query param example

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -43,6 +43,7 @@ export default {
   },
   created() {
     this.fetchTeamMembers(this.teamId);
+    console.log(this.$route.query);
   },
   watch: {
     teamId(newTeamId) {
--- src/components/teams/TeamsItem.vue
+++ src/components/teams/TeamsItem.vue
@@ -11,7 +11,11 @@ export default {
   props: ['id', 'name', 'memberCount'],
   computed: {
     navigateToTeamMembers() {
-      return { name: 'team-members', params: { teamId: this.id } };
+      return {
+        name: 'team-members',
+        params: { teamId: this.id },
+        query: { sort: 'asc' },
+      };
     },
   },
 };
